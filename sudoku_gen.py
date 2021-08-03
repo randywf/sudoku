af8b7ae14f0c89848e104241b12400d2d5edbd6f
randywf <5883513+randywf@users.noreply.github.com>
2021-08-02 21:23:05 -0600

    Changed set_values and count methods - counts are now dictionaries stored as attributes of Board - set_values now updates count dictionaries when a new value is entered - load_csv now uses set_value instead - updated main method for new tests

--- sudoku_gen.py
+++ sudoku_gen.py
@@ -4,10 +4,21 @@ class Board:
     def __init__(self, size):
         self.size = size
         self.side = size * size
+        # 2D Numpy array that stores all values on board (row, col indexed)
         self.board = np.full((self.side, self.side), 0, dtype="int")
+        # Dictionaries to access values
         self.rows = {m: self.board[m, :] for m in range(self.side)}
         self.columns = {n : self.board[:, n] for n in range(self.side)}
         self.squares = {s: self.get_square(s) for s in range(self.side)}
+        # Dictionaries to store counts of values
+        self.counts_rows = {}
+        self.counts_columns = {}
+        self.counts_squares = {}
+        for i in range(self.side):
+            for j in range(self.side):
+                self.counts_rows[(i, j + 1)] = 0
+                self.counts_columns[(i, j + 1)] = 0
+                self.counts_squares[(i, j + 1)] = 0
                 
     
     def get_square(self, s):
@@ -23,7 +34,25 @@ class Board:
 
 
     def set_value(self, m, n, v):
-        s = (n // self.size) + (m * self.size)
+        # If same value, return
+        if (v == self.board[m, n]):
+            return
+
+        s = (n // self.size) + (m // self.size * self.size)
+        
+
+        # Update counts
+        # Changing a value, decrement count of previous value
+        if (self.board[m, n] != 0):
+            self.counts_rows[m, self.board[m, n]] -= 1
+            self.counts_columns[n, self.board[m, n]] -= 1
+            self.counts_squares[s, self.board[m, n]] -= 1
+        # Increment count of new value
+        
+        self.counts_rows[m, v] += 1
+        self.counts_columns[n, v] += 1
+        self.counts_squares[s, v] += 1
+
         self.board[m, n] = v
         self.squares[s] = self.get_square(s)
 
@@ -35,25 +64,7 @@ class Board:
             for i, row in enumerate(rows):
                 nums = row.rstrip().split(',')
                 for j, num in enumerate(nums):
-                    self.board[i,j] = int(num)
-        # Update squares
-        for s in range(self.side):
-            self.squares[s] = self.get_square(s)
-
-    
-    def count_row(self, m):
-        count = np.zeros(self.side)
-        return count
-    
-
-    def count_column(self, m):
-        count = np.zeros(self.side)
-        return count
-    
-
-    def count_square(self, m):
-        count = np.zeros(self.side)
-        return count
+                    self.set_value(i, j, int(num))
 
 
     def validate_row(self, m):
@@ -140,8 +151,22 @@ class Board:
 if __name__ == "__main__":
     test_board = Board(2)
     test_board.load_csv("4x4_valid.csv")
+
     test_board.print()
     print(test_board.rows[0])
     print(test_board.columns[0])
     print(test_board.squares[0])
+    print(test_board.counts_rows)
+    print(test_board.counts_columns)
+    print(test_board.counts_squares)
+    print(test_board.validate())
+
+    test_board.set_value(0,0,1)
+    test_board.print()
+    print(test_board.rows[0])
+    print(test_board.columns[0])
+    print(test_board.squares[0])
+    print(test_board.counts_rows)
+    print(test_board.counts_columns)
+    print(test_board.counts_squares)
     print(test_board.validate())

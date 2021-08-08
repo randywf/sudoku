7cf2896b0d2696323a76b8851982b66f3d3f4c31
randywf <5883513+randywf@users.noreply.github.com>
2021-08-08 10:22:45 -0600

    Added solver class and test class

---
+++ solver.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sudoku import Board
+
+
+
+class Solver(Board):
+    def __init__(self, size):
+        Board.__init__(self, size)
+
+
+
+if __name__ == '__main__':
+    t = Solver(3)
+    print(t.available_cells)
+    t.start()
+    print(t.available_cells)
+    print(t.history)
+    t.print()

0cd64fdfbd0e3ed2596efacdb8b4de7bdab4c886
randywf <5883513+randywf@users.noreply.github.com>
2021-11-09 16:14:31 -0700

    Updated solver.py -Renamed to lowercase -Updated for board rename -Removed whitespace -Removed main -Added "scoring" methods (checking rows, etc.)

--- solver.py
+++ solver.py
@@ -1,21 +1,18 @@
 import random
 import numpy as np
 from numpy.core.fromnumeric import sort
-from board import Board
+from board import board
+from time import time
 
 
 
-class Solver(Board):
-    
+class solver(board):
     def __init__(self, size):
         Board.__init__(self, size)
         self.available_cells = {}
-
         for m in range(self.side):
             for n in range(self.side):
-                # True value is just a placeholder, we're interested in keys
                 self.available_cells[(m,n)] = True
-
         row_ind = np.arange(9)
         col_ind = np.arange(9)
         np.random.shuffle(row_ind)
@@ -24,7 +21,6 @@ class Solver(Board):
             self.set_value(row_ind[i], col_ind[i], i)
             self.available_cells.pop((row_ind[i], col_ind[i]))
     
-
     def next_available_cell(self):
         if len(self.available_cells) == 0:
             return (-1,-1)
@@ -33,7 +29,6 @@ class Solver(Board):
             cells.sort(key=lambda y: y[0])
             return cells[0]
     
-
     def is_value_allowed(self, m, n, v):
         s = self.get_square(m, n)
         if self.counts_rows[m, v] != 0:
@@ -44,12 +39,9 @@ class Solver(Board):
             return False
         return True
     
-
     def solve(self):
         cell = self.next_available_cell()
-
         if cell == (-1,-1):
-            # Make assignment
             for c in self.counts_rows.items():
                 if c[1] == 0:
                     m = c[0][0]
@@ -59,23 +51,16 @@ class Solver(Board):
                     n = c[0][0]
             self.set_value(m, n, v)
             return True
-        
         for i in range(1, self.side + 1):
             if self.is_value_allowed(cell[0], cell[1], i):
-                # Make assignment
                 self.set_value(cell[0], cell[1], i)
                 self.available_cells.pop((cell[0], cell[1]))
-                # Recurse to check if this branch of search works
                 if self.solve():
                     return True
-                # Didn't work, undo assignment
                 self.reset_value(cell[0], cell[1])
                 self.available_cells[(cell[0], cell[1])] = True
-        
-        # Backtrack up the search tree
         return False
     
-
     def swap_numbers(self):
         shuffled_numbers = np.arange(1, self.side + 1)
         np.random.shuffle(shuffled_numbers)
@@ -83,24 +68,18 @@ class Solver(Board):
             for n in range(self.side):
                 new_value = shuffled_numbers[self.board[m, n] - 1]
                 self.board[m, n] = new_value
+    
+    def check_row(self, row):
+        for n in range(1,9):
+            if n not in self.grid:
+                return False
+        return True
 
+    def check_col(self, col):
+        pass
 
-if __name__ == '__main__':
-    t = Solver(3)
-    t.print()
-    t.solve()
-    t.print()
-    if t.validate():
-        print("Valid solution generated")
-    else:
-        print("Invalid solution")
+    def check_box(self, s):
+        pass
 
-    t.swap_numbers()
-    t.print()
-    print("Numbers have been swapped")
-
-    t.recount_values()
-    if t.validate():
-        print("Solution is still valid after recount")
-    else:
-        print("Error")
+    def check_grid(self):
+        pass
